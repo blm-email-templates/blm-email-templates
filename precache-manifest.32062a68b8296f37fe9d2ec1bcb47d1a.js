98d56bef14b15b3b263edfa68ba330af604a2d6f
blm-email-templates <bob@example.com>
2020-06-07 12:49:35 -0500

    Updates

---
+++ precache-manifest.32062a68b8296f37fe9d2ec1bcb47d1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ef3d811da8d64180676ada6b5b2d416",
+    "url": "/blm-email-templates/index.html"
+  },
+  {
+    "revision": "6bedacee7fbb9891094b",
+    "url": "/blm-email-templates/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f64e60b9ed938c84245f",
+    "url": "/blm-email-templates/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6bedacee7fbb9891094b",
+    "url": "/blm-email-templates/static/js/2.ea230f25.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/blm-email-templates/static/js/2.ea230f25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f64e60b9ed938c84245f",
+    "url": "/blm-email-templates/static/js/main.fc86c3be.chunk.js"
+  },
+  {
+    "revision": "8b5eaa2b95301978d792",
+    "url": "/blm-email-templates/static/js/runtime-main.3772b558.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d65a71df04331797cf574773f3567ed5.js"
+  "/blm-email-templates/precache-manifest.32062a68b8296f37fe9d2ec1bcb47d1a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/blm-email-templates/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
